83073592599ba605d680b22044bef965acdd234f
Shayan <shayanf1001@gmail.com>
2021-02-23 21:26:03 -0500

    Fixed fade in randomize, updated projects.html

--- scripts.js
+++ scripts.js
@@ -1,21 +1,27 @@
-
-    var x = 0;
+var x = 0;
 function randomize(){
     y = x;
     x = Math.floor(Math.random() * 3);
+    var element = document.getElementById("randMess");
+
     while (y == x) {
         x = Math.floor(Math.random() * 3);
     }
 
-    if (x == 0){
-        document.getElementById('randMess').innerHTML = 'Programmer';
-    }
+    element.classList.add("fade-out");
 
-    else if (x == 1){
-        document.getElementById('randMess').innerHTML = 'Software Developer';
-    }
-
-    else {
-        document.getElementById('randMess').innerHTML = 'Computer Scientist';
-    }
+    setTimeout(() => {
+        if (x == 0){
+            element.innerHTML = 'Programmer';
+        }
+    
+        else if (x == 1){
+            element.innerHTML = 'Software Developer';
+        }
+    
+        else {
+            element.innerHTML = 'Computer Scientist';
+        }
+    }, 400);
+    setTimeout(()=>{element.classList.remove("fade-out")}, 400);
 }
